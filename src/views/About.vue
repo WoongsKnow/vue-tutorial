0e2d21890897c592863510262b639a47465b6b88
Woong Park <woongsknow@gmail.com>
2020-10-04 02:36:04 +0900

    [UPDATE] 기본 Vue 프로젝트 셋팅

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <!-- TODO -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    // FIXME : 데이터에 문제가 있는 것 같음. 확인 필요 @WoongPark
+    return {
+
+    }
+  }
+};
+</script>
